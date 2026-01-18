8e291bc3d4cc4c0b4fdf20faf28f1ddd44b12661
yeadr <yussef.elalami@institutvidreres.cat>
2026-01-18 13:30:47 +0100

    Pagina migrada falta responsive

--- src/app/componentes/header/header.ts
+++ src/app/componentes/header/header.ts
@@ -1,17 +1,25 @@
 import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
+import { Getasset } from '../../serveis/getasset/getasset';
+import { Getroute } from '../../serveis/getroute/getroute';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
 export class Header {
 
-  usuario: any;
-
-  constructor() {
-
+  constructor(private getasset: Getasset, private getroute: Getroute){
   }
 
+  public cgetpath(path: string){
+    return this.getasset.getpath(path);
+  }
+
+  public cgetroute(path: string){
+    return this.getroute.getroute(path);
+  }
+  
 }
