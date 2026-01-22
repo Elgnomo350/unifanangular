a26f703bcc3894d2667d5816253126b9ad5809fa
Adria Imaz <adriaimaz03@gmail.com>
2026-01-22 08:50:51 +0100

    inici-sesio-if

--- src/app/componentes/header/usuaridades.ts
+++ src/app/componentes/header/usuaridades.ts
@@ -4,5 +4,25 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class Usuaridades {
-  
+  registrat1 = false
+  Usuari1 : string
+  Cognom1 : string
+  Correu : string
+  passwd : string
+  direccio : string
+  Telefon : bigint
+
+
+  constructor(usr1:string, cog:string, correu:string, pswd:string, direc:string, tel:bigint) {
+    this.Usuari1 = usr1
+    this.Cognom1 = cog
+    this.Correu = correu
+    this.passwd = pswd
+    this.direccio = direc
+    this.Telefon = tel
+  }
+
+  public getUserNom () {
+    return this.Usuari1
+  }
 }
