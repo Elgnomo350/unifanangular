c3dd12fd6e3e42809b20c24ad740f97a3120bea2
Adria Imaz <adriaimaz03@gmail.com>
2026-01-24 23:59:53 +0100

    Inici-usuari-bastante-terminat

--- src/app/componentes/cistella/cistella.ts
+++ src/app/componentes/cistella/cistella.ts
@@ -1,6 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Getroute } from '../../serveis/getroute/getroute';
-import { RouterLink } from "@angular/router";
+import { Router, RouterLink } from "@angular/router";
+import { Manejarcistella } from '../../serveis/manejarcistella/manejarcistella';
+import { IniciarSessio } from '../../serveis/usuaridades/usuaridades';
+
 
 @Component({
   selector: 'app-cistella',
@@ -8,14 +11,57 @@ import { RouterLink } from "@angular/router";
   templateUrl: './cistella.html',
   styleUrl: './cistella.css',
 })
-export class Cistella {
+export class Cistella implements OnInit{
 
-    constructor(private getroute: Getroute){
+  private total: number = 0
+ 
+    constructor(private getroute: Getroute, private manejarcistella: Manejarcistella, private iniciaSessio: IniciarSessio,
+      private router: Router
+    ){
     }
+
+  ngOnInit(): void {
+    if(!this.iniciaSessio.getIniciatSessio()){
+    alert("Inicia sesion para tener una cesta")
+    this.router.navigate(['/registre']);
+    }
+  }
   
     public cgetroute(path: string){
       return this.getroute.getroute(path);
     }
-  
 
+    public getcistella(){
+      return this.manejarcistella.gcistella
+    }
+
+    public sumTotal(){
+      this.total = 0
+      for (let index = 0; index < this.manejarcistella.gcistella.length; index++) {
+        this.total += this.manejarcistella.gcistella[index].gpreutotal
+      }
+
+      document.getElementById("total")!.textContent = "Total: " + this.retornarPreuArrodonit(this.total) + "€"
+    }
+
+    public retornarPreuArrodonit(num: number){
+      return Math.round(num * 100) / 100
+    }
+
+    public buidarcistella(){
+      this.manejarcistella.buidarcistella()
+    }
+
+    public borrarelement(index: number){
+      this.manejarcistella.eliminarProducto(index)
+    }
+
+    public sumarUnaUnitat(index: number){
+      this.manejarcistella.gcistella.at(index)?.sumar(1)
+    }
+
+
+    public restarUnaUnitat(index: number){
+      this.manejarcistella.gcistella.at(index)?.sumar(-1)
+    }
 }
--- src/app/componentes/index/index.ts
+++ src/app/componentes/index/index.ts
@@ -1,18 +1,24 @@
 import { Component } from '@angular/core';
 import { Getasset } from '../../serveis/getasset/getasset';
+import { RouterLink } from "@angular/router";
+import { Getroute } from '../../serveis/getroute/getroute';
 
 @Component({
   selector: 'app-index',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './index.html',
   styleUrl: './index.css',
 })
 export class Index {
 
-constructor(private getasset: Getasset){}  
+constructor(private getasset: Getasset, private getroute: Getroute){}  
 
   public cgetpath(path: string){
     return this.getasset.getpath(path);
+  }
+
+  public cgetroute(path: string){
+    return this.getroute.getroute(path);
   }
 
 }
