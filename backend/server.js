9ff09d5b481a68261fbccfff0c1aa6bb604efc7e
yeadr <yussef.elalami@institutvidreres.cat>
2026-02-23 11:42:25 +0100

    Crear server

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const cors = require('cors');
+
+
+app.use(cors());
+const app = express();
+
+app.listen(23000, () => console.log(`listening on http://localhost:${23000}`));
+
+
+
+
