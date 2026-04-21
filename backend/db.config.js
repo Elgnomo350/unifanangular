a57613646545372d5a3ecb8c5c1ebcefd04c41e2
yeadr <yussef.elalami@institutvidreres.cat>
2026-04-21 09:53:45 +0200

    testsAct4YussefElAlami

--- backend/db.config.js
+++ backend/db.config.js
@@ -9,6 +9,12 @@ const configBBDD = () => {
       rejectUnauthorized: false
     }
   },
+  pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+  },
   logging: false
 });
 }
